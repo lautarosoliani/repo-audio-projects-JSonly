48c8c252d579063e39143e55f3f245d3543cc4ac
lautarosoliani <lautarosoliani@hotmail.com>
2022-09-23 12:14:04 +0200

    ESlint changes

---
+++ JS/components/Header/Top_Header/logo.js
@@ -0,0 +1,10 @@
+
+export default function Image () {
+  return (
+    <img
+      src='C:/Users/Dell/Desktop/Stackwork/audio-projects-JSonly/JS/components/Header/Top_Header/Logo.png'
+      alt='Logo Stack'
+      style='width: 103px; height: 23px'
+    />
+  )
+}
--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,31 @@
-console.log('test')
+// import Image from './components/Header/Top_Header/logo'
+
+// console.log('test')
+// Image()
+
+// const funcionImage = () => {
+//   return (
+//     <img
+//       src='C:/Users/Dell/Desktop/Stackwork/audio-projects-JSonly/JS/Logo.png'
+//       alt='Logo Stack'
+//       style='width: 103px; height: 23px'
+//     />
+//   )
+// }
+
+// funcionImage()
+
+// function showImage (src, width, height, alt) {
+//   const img = document.createElement('img')
+//   img.src = src
+//   img.width = width
+//   img.height = height
+//   img.alt = alt
+
+//   // This next line will just add it to the <body> tag
+//   document.body.appendChild(img)
+// }
+
+// showImage()
+
+<img src='...' class='img-fluid' alt='Responsive image' />
